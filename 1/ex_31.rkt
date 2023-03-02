f4b78df4b63c5be3eb5ce64f563b757323deed51
John Willard <willard.john.gray@gmail.com>
2023-03-01 21:48:18 -0500

    ex 1.31 first pass (product function)

---
+++ 1/ex_31.rkt
@@ -0,0 +1,26 @@
+#lang sicp
+
+(define (identity x) x)
+
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+         (product term (next a) next b))))
+
+(define (factorial n)
+  (product identity 1 inc n))
+
+(factorial 5)
+
+(define (product-iter term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* (term a) result))))
+  (iter a 1))
+
+(define (factorial-iter n)
+  (product identity 1 inc n))
+
+(factorial-iter 5)
