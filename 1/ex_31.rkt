5ad9aa7b6af2b9613f7000ff57158a03c7a1eceb
John Willard <willard.john.gray@gmail.com>
2023-03-04 13:16:41 -0500

    finish 1.31 (approximating pi)

--- 1/ex_31.rkt
+++ 1/ex_31.rkt
@@ -21,6 +21,31 @@
   (iter a 1))
 
 (define (factorial-iter n)
-  (product identity 1 inc n))
+  (product-iter identity 1 inc n))
 
 (factorial-iter 5)
+
+(define (approximate-pi n)
+
+  (define (upper-term n)
+    (if (even? n)
+        (+ n 2)
+        (+ n 1)))
+
+  (define (lower-term n)
+    (if (even? n)
+        (+ n 1)
+        (+ n 2)))
+
+  (* 4.0 (/
+   (product-iter upper-term 1 inc n)
+   (product-iter lower-term 1 inc n))))
+
+(approximate-pi 10)
+; 3.2751010413348074
+(approximate-pi 100)
+; 3.1570301764551676
+(approximate-pi 1000)
+; 3.1431607055322663
+(approximate-pi 10000)
+; 3.1417497057380523
