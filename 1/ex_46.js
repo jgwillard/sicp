edebb012074dd640230cf3ff03614cfbff5a0ca2
John Willard <willard.john.gray@gmail.com>
2023-03-30 21:42:00 -0400

    solve exercise 1.46 in js

---
+++ 1/ex_46.js
@@ -0,0 +1,42 @@
+const tolerance = 0.00001
+
+function iterativeImprove(isGoodEnough, improve) {
+
+    const _iterativeImprove = (guess) => {
+        if (isGoodEnough(guess)) {
+            return guess
+        }
+        return _iterativeImprove(improve(guess))
+    }
+    return _iterativeImprove
+}
+
+function fixedPoint(f, firstGuess) {
+
+    const isGoodEnough = (guess) => {
+        const next = f(guess)
+        return Math.abs(guess - next) < tolerance
+    }
+
+    return iterativeImprove(isGoodEnough, f)(firstGuess)
+}
+
+console.log(fixedPoint(Math.cos, 1.0))
+
+function sqrt(x) {
+
+    const isGoodEnough = (guess) => {
+        return Math.abs(guess * guess - x) < tolerance
+    }
+
+    const average = (x, y) => (x + y) / 2
+
+    const improve = (guess) => {
+        return average(guess, x / guess)
+    }
+
+    return iterativeImprove(isGoodEnough, improve)(1.0)
+}
+
+console.log(sqrt(9))
+
